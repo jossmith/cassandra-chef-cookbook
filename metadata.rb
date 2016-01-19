6de97b210295125c75c26f73a192f3148dbc9391
jossmith <jossmith@users.noreply.github.com>
2016-01-19 13:20:06 -0500

    fixes for upgrade

--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,7 @@ maintainer_email 'michael@clojurewerkz.org'
 license 'Apache 2.0'
 description 'Installs/configures Apache Cassandra'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-<<<<<<< HEAD
 version '5.3'
-=======
-version '4.2.0'
->>>>>>> refs/remotes/michaelklishin/master
 depends 'java'
 depends 'ulimit'
 depends 'apt'
